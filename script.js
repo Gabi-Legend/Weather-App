3b57b786000a56eb88f3a005735e942f977d9e5c
Gabi-Legend <gabrielmazuru6@gmail.com>
2025-06-28 15:33:00 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-let input = document.getElementById("inputText");
-let displayElement = document.getElementById("displayText");
-displayElement.innerHTML = localStorage.getItem("value");
-input.addEventListener("keyup", display);
+let searchButton = document.getElementById("searchButton");
 
-function display() {
-  localStorage.setItem("value", input.value);
-  displayElement.innerHTML = localStorage.getItem("value");
+searchButton.addEventListener("click", searchCity);
+
+function searchCity() {
+  let city = document.getElementById("city").value;
+  const apiKey = "d7e498e89d084db3809111856252706"; // înlocuiește cu cheia ta
+
+  if (!city) return alert("Please enter a city name!");
+
+  fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}`)
+    .then((response) => response.json())
+    .then((data) => {
+      let searchedCity = document.getElementById("searchedCity");
+      let weatherImage = document.getElementById("weatherImage");
+      let temperature = document.getElementById("temperature");
+
+      searchedCity.innerText = `${data.location.name}, ${data.location.country}`;
+      weatherImage.src = "https:" + data.current.condition.icon;
+      weatherImage.alt = data.current.condition.text;
+      temperature.innerText = `Temperature: ${data.current.temp_c}°C`;
+      weatherImage.style.display = "inline-block";
+    })
+    .catch((error) => {
+      console.error("Eroare:", error);
+      alert("City not found or API error.");
+    });
 }
